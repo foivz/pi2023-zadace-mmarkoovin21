5c436c7770b0e5eadf5474952869c640dea87e59
Magdalena Markovinović <mmarkovin21@foi.hr>
2023-05-19 23:40:24 +0200

    Prva implementacija CleanWorks Schedulera Napravljeno početno grafičko sučelje sa dva gumba

---
+++ Software/Treća_Zadaća/CleanWorks Scheduler/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CleanWorks_Scheduler
+{
+    partial class FrmWorkers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddWorker = new System.Windows.Forms.Button();
+            this.btnGenerateReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddWorker
+            // 
+            this.btnAddWorker.Location = new System.Drawing.Point(71, 44);
+            this.btnAddWorker.Name = "btnAddWorker";
+            this.btnAddWorker.Size = new System.Drawing.Size(113, 40);
+            this.btnAddWorker.TabIndex = 0;
+            this.btnAddWorker.Text = "+ Dodaj";
+            this.btnAddWorker.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerateReport
+            // 
+            this.btnGenerateReport.Location = new System.Drawing.Point(768, 39);
+            this.btnGenerateReport.Name = "btnGenerateReport";
+            this.btnGenerateReport.Size = new System.Drawing.Size(150, 51);
+            this.btnGenerateReport.TabIndex = 1;
+            this.btnGenerateReport.Text = "Generiraj izvješće o radnicima";
+            this.btnGenerateReport.UseVisualStyleBackColor = true;
+            // 
+            // FrmWorkers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 554);
+            this.Controls.Add(this.btnGenerateReport);
+            this.Controls.Add(this.btnAddWorker);
+            this.Name = "FrmWorkers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Evidencija radnika";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddWorker;
+        private System.Windows.Forms.Button btnGenerateReport;
+    }
+}
+
